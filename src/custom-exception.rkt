1c1af0bde731a2cd66a6e65d390e53296604bf24
Vicente Ruiz Escobar <greatdazz3@gmail.com>
2026-04-18 19:37:34 -0400

    control en input de numeros en operaciones

--- src/custom-exception.rkt
+++ src/custom-exception.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
-(provide (struct-out division-por-cero-error))
+(provide (struct-out division-por-cero-error) (struct-out entrada-no-numerica-error))
 
 ; Excepción personalizada
 ; Extiende exn:fail:user
 (struct division-por-cero-error exn:fail:user ())
+(struct entrada-no-numerica-error exn:fail:user ())
